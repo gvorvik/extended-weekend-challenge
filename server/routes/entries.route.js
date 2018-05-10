6d61eb0ee8aaad3e1e370c6e7ce4fde63adc3488
Greg Orvik <gvorvik@gmail.com>
2018-05-10 18:03:17 -0500

    calculate the hours worked on the server side

--- server/routes/entries.route.js
+++ server/routes/entries.route.js
@@ -17,21 +17,19 @@ router.post('/', (req, res) => {
         ("entry_name", "date", "total_hours", "project_id")
         VALUES ($1, $2, $3, $4)`;
 
+        //Calculate hours worked
         let hours = calcHours(entryToAdd.start, entryToAdd.end);
-        entryToAdd.hours = hours.toFixed(2);
-
-
-
-
-        // pool.query(queryText, [entryToAdd.entry_name, entryToAdd.date,
-        //      entryToAdd.hours, entryToAdd.project_id])
-        // .then((response) => {
-        //     res.sendStatus(200);
-        // })
-        // .catch((error) => {
-        //     res.sendStatus(500);
-        //     console.log(error);        
-        // });
+        entryToAdd.hours = Number(hours.toFixed(2));
+        
+        pool.query(queryText, [entryToAdd.entry_name, entryToAdd.date,
+             entryToAdd.hours, entryToAdd.project_id])
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            res.sendStatus(500);
+            console.log(error);        
+        });
     }
 );
 
@@ -50,14 +48,15 @@ router.delete('/:id', (req, res) => {
 
 //function to calculate hours
 function calcHours(start, end) {
+    //Break apart time into hours and minutes
     let startArray = start.split(':');
     let endArray = end.split(':');
-    let startHour = Number(startArray[0]);
-    let endHour = Number(endArray[0]);
-    let startMinute = Number(startArray[1]) + (startHour*60);
-    let endMinute = Number(endArray[1]) + (endHour*60);
-    let hours = (endMinute - startMinute)/60;
-    return hours;
+    //Calculate total minutes for start and end
+    let startMinute = Number(startArray[1]) + (Number(startArray[0])*60);
+    let endMinute = Number(endArray[1]) + (Number(endArray[0])*60);
+    //Return Hours Worked
+    let hoursWorked = (endMinute - startMinute)/60;
+    return hoursWorked;
 };
 
 module.exports = router;
