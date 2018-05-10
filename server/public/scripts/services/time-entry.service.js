543ee366d2c3514817a286171d0b89fac0518e9b
Greg Orvik <gvorvik@gmail.com>
2018-05-10 10:44:13 -0500

    make get and post project requests to server

--- server/public/scripts/services/time-entry.service.js
+++ server/public/scripts/services/time-entry.service.js
@@ -7,12 +7,38 @@ app.service('TimeEntryService', ['$http', function($http) {
         projects: []
     };
 
+    self.newProject = {
+        name: 'test',
+        sqft: 0,
+    };
+
     self.getProjects = function() {
         console.log('button clicked');
+        $http({
+            method: 'GET',
+            url: '/projects'
+        })
+        .then(function(results) {
+            console.log(results);
+        })
+        .catch(function(error) {
+            console.log(`error in get request ${error}`);
+        });
     };
 
     self.postProject = function() {
         console.log('button clicked');
+        $http({
+            method: 'POST',
+            url: '/projects',
+            data: self.newProject
+        })
+        .then(function(results) {
+            console.log(results);
+        })
+        .catch(function(error) {
+            console.log(`error in get request ${error}`);
+        });
     };
     
 }]);
--- server/server.js
+++ server/server.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
 
+app.get('/projects', (req, res) => {
+    res.send('get got got');
+});
+
+app.post('/projects', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
     console.log(`app listening at port ${PORT}`);
 });
