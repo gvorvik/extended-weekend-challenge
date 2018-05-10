63fc4f0006fc9f25b2bf6b569be33905acfc8558
Greg Orvik <gvorvik@gmail.com>
2018-05-10 09:04:16 -0500

    spin up server on localhost 5000

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log(`app listening at port ${PORT}`);
+})
